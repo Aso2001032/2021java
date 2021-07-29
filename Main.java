4c19eb59d1d48f39ae27303ef02ce359ace7aa7e
Aso2001032 <83050420+Aso2001032@users.noreply.github.com>
2021-07-29 18:07:33 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+package kadai;
+
+public class Main {
+    public static void main(String[] args) {
+        Game game = new Game();
+
+        game.init();
+
+        game.execute();
+    }    
+}
